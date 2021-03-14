ed736c6bd70407e9fdd0bffe8e9c7f03a81db121
airi <airi-love-clover@rakuten.jp>
2021-03-15 00:21:37 +0900

    changed pages keyword

--- src/pages/picupCafe.js
+++ src/pages/picupCafe.js
@@ -5,11 +5,10 @@ import { Loading } from "../components/loading";
 export async function getStaticProps() {
   // export const getStaticProps = async () => {
   const keywords = "猫";
-  // const utf8Key = encodeURIComponent(keywords);
+  const utf8Key = encodeURIComponent(keywords);
 
   const res = await fetch(
-    // `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&genre=G014&keyword=${utf8Key}&count=20&format=json`
-    `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&genre=G014&keyword=${keywords}&count=20&format=json`
+    `http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${process.env.API_KEY}&genre=G014&keyword=${utf8Key}&count=20&format=json`
   );
   const datasLists = await res.json();
   const data = datasLists.results.shop;
